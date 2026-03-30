83b076f00a84fc966ee20a61b62faea27c9aba27
juhairulislam <juhairulislam2018@gmail.com>
2026-03-30 06:57:17 +0600

    font add and theme update

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,7 @@ function App() {
 
   return (
     <>
-
-    <h1 className='text-5xl text-red-500 p-2 font-bold '>Hello world</h1>
-    <button className='btn btn-accent'>daisy</button>
+    
 
     </>
   )
