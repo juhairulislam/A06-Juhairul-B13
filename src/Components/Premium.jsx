10b2329c1c1055a2364eb20767f381b64a03a2e0
juhairulislam <juhairulislam2018@gmail.com>
2026-03-30 17:57:04 +0600

    make two tab toggling

--- src/Components/Premium.jsx
+++ src/Components/Premium.jsx
@@ -1,16 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Premium = () => {
+
+    const [activeTab, setActiveTab] = useState(true) ;
     return (
         <div className='my-20 text-center'>
             <h1 className='text-3xl font-bold'>Premium Digital Tools</h1>
-            <p className='text-[11px] text-[#627382] my-4'>Choose from our curated collection of premium digital products designed <br /> to boost your productivity and creativity.</p>
+            <p className='text-[10px] md:text-[11px] text-[#627382] my-4'>Choose from our curated collection of premium digital products designed <br /> to boost your productivity and creativity.</p>
 
-            <div className='rounded-3xl py-1 px-2  mx-auto w-38 border-1 border-gray-200 flex gap-2'>
-                <button className='px-2 py-1.5 mx-0 bg-linear-to-r from-[#4F39F6]  to-[#9514FA] rounded-4xl text-white'>Products</button>
-                <button className='px-2 py-1.5'>Cart</button>
+ <div className='rounded-3xl py-1  px-2 mx-auto w-44 border-1 border-gray-200 flex gap-2 mt-5'>
+                <button onClick={() => setActiveTab(true)} className={`px-2 py-1.5 mx-0 font-medium ${activeTab===true?'bg-linear-to-r from-[#4F39F6]  to-[#9514FA] text-white' : ''} rounded-4xl `}>Products</button>
+                <button  onClick={() => setActiveTab(false)} className={`px-4 py-1.5 mx-0 font-medium ${activeTab===false?'bg-linear-to-r from-[#4F39F6]  to-[#9514FA] text-white' : ''} rounded-4xl `}>Cart</button>
             </div>
-            
         </div>
 
         
