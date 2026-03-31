9fbe772c1cd1d19cec32273f2486c84393a60879
juhairulislam <juhairulislam2018@gmail.com>
2026-03-31 18:17:41 +0600

    add in card total and proceed...checkout

--- src/Components/Cart.jsx
+++ src/Components/Cart.jsx
@@ -1,6 +1,6 @@
 import React, { use } from 'react';
 
-const Cart = ({ promise,carts }) => {
+const Cart = ({ promise, carts }) => {
 
     const allData = use(promise);
     console.log(allData)
@@ -12,32 +12,44 @@ const Cart = ({ promise,carts }) => {
             {
                 carts.map((cart) => <div key={cart.id} className='p-2 md:p-2'>
 
-                    
+
 
                     <div className='bg-zinc-50 p-1 md:p-4 rounded-xl flex justify-between'>
                         <div className='flex gap-4'>
 
-                       <div className='rounded-full border border-zinc-300 p-3 md:p-4'>
-                         <img className='h-8 md:w-8' src={cart.icon} alt="cart icon" />
+                            <div className='rounded-full border border-zinc-300 p-3 md:p-4'>
+                                <img className='h-8 md:w-8' src={cart.icon} alt="cart icon" />
 
-                       </div>
-                        <div className='pt-2'>
-                            <h1 className='text-[13px] md:text-xl font-semibold mb-1'>{cart.name}</h1>
-                            <p className='text-[14px] text-[#627382]'>{cart.price}</p>
+                            </div>
+                            <div className='pt-2'>
+                                <h1 className='text-[13px] md:text-xl font-semibold mb-1'>{cart.name}</h1>
+                                <p className='text-[14px] text-[#627382]'>{cart.price}</p>
+                            </div>
+
                         </div>
 
-                    </div>
-
-                    <div className='flex items-center'>
-                        <h2 className='text-[12px] md:text-[14px] text-red-400 font-semibold'>Remove</h2>
-                    </div>
+                        <div className='flex items-center'>
+                            <h2 className='text-[12px] md:text-[14px] text-red-400 font-semibold'>Remove</h2>
+                        </div>
                     </div>
 
 
-                </div>)
+
+
+                </div>
+
+                )
             }
-           
-            
+
+            <div className='flex justify-between p-4'>
+                <h1 className='text-xl font-medium'>Total</h1>
+                <p className='text-xl font-medium'>$</p>
+            </div>
+
+            <div className='p-4'>
+                <button className='w-full bg-linear-to-r from-[#4F39F6]  to-[#9514FA] text-white font-semibold p-2 rounded-4xl'>Proceed to Checkout</button>
+            </div>
+
         </div>
     );
 };
